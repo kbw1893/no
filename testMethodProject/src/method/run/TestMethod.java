322124c5649b5118ebb81d9ab5cf1df9c6621efd
kobyungwan <kbw1893@naver.com>
2017-06-06 18:51:50 +0900

    aaaaaaaaa

--- testMethodProject/src/method/run/TestMethod.java
+++ testMethodProject/src/method/run/TestMethod.java
@@ -5,7 +5,7 @@ import method.sample.SampleMethodClass;
 public class TestMethod {
 
 	public static void main(String[] args) {
-		// yyyyy
+		// yyyyy 123456
 		SampleMethodClass samp = new SampleMethodClass();
 		
 		//��ȯ�� ���� �Ű����� ���� �޼ҵ� ����
